98ad7910bf905c30fd9e43ae3f680d1002c0ef03
WingedSeal <firm09719@gmail.com>
2021-09-09 15:03:33 +0700

    Update avalanche testcase

--- tests/test_avalanche_and_time.py
+++ tests/test_avalanche_and_time.py
@@ -4,12 +4,18 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from aesjosephus import avalanche, time
 import unittest
 
-class TestNoErrorEEE(unittest.TestCase):
+class TestNoException(unittest.TestCase):
     def test_avalanche_and_time(self):
         row = 10
         avalanche.avalanche_df(row)
         time.time_df(row)
-        self.assertTrue(True)
+
+class TestAvalancheEffect(unittest.TestCase):
+    def test_example_case(self):
+        self.assertEqual(avalanche.avalanche("abcd","abcd"), 0)
+        self.assertEqual(avalanche.avalanche("abcd","abce"), 1/32)
+        self.assertEqual(avalanche.avalanche("abcd","efgh"), 5/32)
+    
 
 if __name__ == "__main__":
     unittest.main()
